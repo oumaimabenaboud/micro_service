5d0f6d1fa8ba9f45a60f4d6afdc0778a77bebbc9
Oumaïma <120368654+oumaimabenaboud@users.noreply.github.com>
2023-05-16 17:46:52 +0100

    first

--- bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
+++ bank-account-service/src/main/java/org/sid/bankaccountservice/web/AccountRestController.java
@@ -1,20 +1,34 @@
 package org.sid.bankaccountservice.web;
 
+import lombok.AllArgsConstructor;
+import org.sid.bankaccountservice.dto.BankAccountRequestDTO;
+import org.sid.bankaccountservice.dto.BankAccountResponseDTO;
 import org.sid.bankaccountservice.entities.BankAccount;
+import org.sid.bankaccountservice.mappers.AccountMapper;
 import org.sid.bankaccountservice.repositories.BankAccountRepository;
+import org.sid.bankaccountservice.service.AccountService;
+import org.sid.bankaccountservice.service.AccountServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+
+
 @RestController
+@RequestMapping("/api")
 public class AccountRestController {
     private BankAccountRepository bankAccountRepository;
-
-    public AccountRestController (BankAccountRepository bankAccountRepository){
-        this.bankAccountRepository=bankAccountRepository;
+    private AccountService accountService;
+    private AccountMapper accountMapper;
+    @Autowired
+    public AccountRestController(AccountService accountService) {
+        this.accountService = accountService;
     }
+
+
     @GetMapping("/bankAccounts")
     public List<BankAccount> bankAccounts(){
         return bankAccountRepository.findAll();
@@ -25,9 +39,8 @@ public class AccountRestController {
                 .orElseThrow(()->new RuntimeException(String.format("Account %s not found",id)));
     }
     @PostMapping("/bankAccounts")
-    public BankAccount save (@RequestBody BankAccount bankAccount){
-        if(bankAccount.getId()==null) bankAccount.setId(UUID.randomUUID().toString());
-        return bankAccountRepository.save(bankAccount);
+    public BankAccountResponseDTO save (@RequestBody BankAccountRequestDTO requestDTO){
+         return accountService.addAccount(requestDTO);
     }
 
     @PutMapping("/bankAccounts/{id}")
